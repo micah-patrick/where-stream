5201e64072e6db987830d8758e7b722c3f747d84
Micah <mbro36@gmail.com>
2021-05-03 17:06:32 -0600

    temp 404 page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/common/Header';
+import NotFound from './components/common/NotFound';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <NotFound />
     </div>
   );
 }
---
+++ src/components/common/NotFound.js
@@ -0,0 +1,11 @@
+
+export default function NotFound() {
+    return(
+        <div className="notFound">
+            <h2>
+                404
+            </h2>
+            <p>This page is not found</p>
+        </div>
+    )
+}
