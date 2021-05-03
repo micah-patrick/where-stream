5897792d5975f9abf73b58a9949013771fa3c3f9
Micah <mbro36@gmail.com>
2021-05-03 16:53:28 -0600

    work on header

---
+++ src/components/common/Header.js
@@ -0,0 +1,10 @@
+
+export default function Header() {
+    return(
+        <header className="App-header">
+            <h1>
+            Where Stream
+            </h1>
+        </header>
+    )
+}
