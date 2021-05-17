8135b053df3f79937501468c03a2fe8f34471db5
Micah <mbro36@gmail.com>
2021-05-17 11:28:44 -0600

    not found route from /sources/:sourceId

--- src/components/sources/SourcePage.js
+++ src/components/sources/SourcePage.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import { Link, useParams } from "react-router-dom";
+import { useEffect, useState } from 'react';
+import NotFound from "../common/NotFound";
 
 export default function SourcePage({sources}){
 
+    const [display, setDisplay] = useState(<NotFound />)
     const {sourceId} = useParams();
     const source = sources.find((source) => source.id === Number(sourceId));
     // const source = sources[0];
     console.log(sourceId);
 
+    useState(() =>{
+        if (source){
+            setDisplay(
+                <div className="p-0" style={{}}>
+                    <img style={{}} src={`https://plchldr.co/i/1000x100?text=${source.name}&bg=${source.color}`} className="card-img-top" />
+                </div>
+            )
+        }
+    },[])
+
+
     return (
-        <div className="p-0" style={{}}>
-                <img style={{}} src={`https://plchldr.co/i/1000x100?text=${source.name}&bg=${source.color}`} className="card-img-top" />
-        </div>
+        display
     )
 }
