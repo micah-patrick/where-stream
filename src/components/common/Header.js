8eba07d84fc187041a26a2ded23779889efbf5be
Micah <mbro36@gmail.com>
2021-05-18 23:30:27 -0600

    link logo to home

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,10 +1,19 @@
+import { Link, useParams } from "react-router-dom";
 
 export default function Header() {
+
+    const headerLinkStyles = {
+        textDecoration: "none",
+        color: "#fff"
+    }
+
     return(
         <header className="App-header">
-            <h1>
-            Where Stream
-            </h1>
+            <Link to="/" style={headerLinkStyles}>
+                <h1>
+                Where Stream
+                </h1>
+            </Link>
         </header>
     )
 }
